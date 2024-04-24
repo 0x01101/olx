b7bb16a28de97efb971cd7f224e2053858fd86ee
JamJestJerzy <github@j3rzy.dev>
2024-04-24 19:54:53 +0200

    Updated SQL, added fetch methods

--- app/lib/data/sql.ts
+++ app/lib/data/sql.ts
@@ -3,12 +3,14 @@ import { Pool, PoolConnection, QueryOptions } from "mariadb";
 import config from "@/config.json";
 import {
   Bid,
-  Category, EMail, JoinedProduct,
+  Category,
   Product,
-  RawUser, Role, Transaction,
+  RawBidRecord,
+  RawProductRecord,
+  RawTransactionRecord,
+  Transaction,
   User,
 } from "@/app/lib/definitions";
-import { expectContextual } from "sucrase/dist/types/parser/traverser/util";
 
 const pool: Pool = mariadb.createPool( {
   host:     config.MySQLHost,
@@ -38,58 +40,210 @@ async function execQuery ( sql: string | QueryOptions, values?: any ): Promise<a
 
 export async function fetchUsers (): Promise<User[]>
 {
-  const rows: RawUser[] = await execQuery( `SELECT *
-                                            FROM users` );
-  return rows.map( ( rawUser: RawUser ): User => ( {
-    ...rawUser,
-    email: rawUser.email as EMail,
-    role:  rawUser.role as Role,
+  const query: string = `
+      SELECT id, username, email, password, role, created_at
+      FROM users;
+  `;
+  const rows: User[] = await execQuery( query );
+  return rows.map( ( row: User ): User => ( {
+    ...row,
+    password: row.password,
   } ) );
 }
 
 export async function fetchCategories (): Promise<Category[]>
 {
-  return await execQuery( `SELECT *
-                           FROM categories` );
+  const query: string = `
+      SELECT id, name, created_at
+      FROM categories;
+  `;
+  return await execQuery( query );
 }
 
 export async function fetchProducts (): Promise<Product[]>
 {
   const query: string = `
-      SELECT p.id          AS id,
-             p.name        AS name,
-             p.description AS description,
-             p.price       AS price,
-             p.category_id AS category_id,
-             p.created_at  AS created_at,
-             c.name        AS category_name,
-             c.created_at  AS category_created_at
-      FROM products p
-               JOIN
-           categories c ON p.category_id = c.id;
+      SELECT products.id,
+             products.name,
+             products.description,
+             products.price,
+             products.created_at,
+             categories.id         as category_id,
+             categories.name       as category_name,
+             categories.created_at as category_created_at,
+             users.id              as seller_id,
+             users.username        as seller_username,
+             users.email           as seller_email,
+             users.password        as seller_password,
+             users.role            as seller_role,
+             users.created_at      as seller_created_at
+      FROM products
+               INNER JOIN categories ON products.category_id = categories.id
+               INNER JOIN users ON products.user_id = users.id;
   `;
-  const rows: JoinedProduct[] = await execQuery( query );
-  return rows.map( ( joinedProduct: JoinedProduct ): Product => ( {
-    id:          joinedProduct.id,
-    seller: {
-    
-    },
-    name:        joinedProduct.name,
-    description: joinedProduct.description,
-    price:       joinedProduct.price,
+  const rows: RawProductRecord[] = await execQuery( query );
+  return rows.map( ( row: RawProductRecord ): Product => ( {
+    id:          row.id,
+    name:        row.name,
+    description: row.description,
+    price:       row.price,
+    created_at:  row.created_at,
     category:    {
-      id:         joinedProduct.category_id,
-      name:       joinedProduct.category_name,
-      created_at: joinedProduct.category_created_at,
+      id:         row.category_id,
+      name:       row.category_name,
+      created_at: row.category_created_at,
     },
-    created_at:  joinedProduct.created_at,
+    seller:      {
+      id:         row.seller_id,
+      username:   row.seller_username,
+      email:      row.seller_email,
+      password:   row.seller_password,
+      role:       row.seller_role,
+      created_at: row.seller_created_at,
+    },
   } ) );
 }
 
-export async function fetchBids (): Promise<Bid[]> {
+export async function fetchBids (): Promise<Bid[]>
+{
   const query: string = `
-    SELECT
-    FROM bids b
-    JOIN
-  `
+      SELECT bids.id,
+             bids.amount,
+             bids.created_at,
+             users.id              as user_id,
+             users.username,
+             users.email,
+             users.password,
+             users.role,
+             users.created_at      as user_created_at,
+             products.id           as product_id,
+             products.name         as product_name,
+             products.description  as product_description,
+             products.price        as product_price,
+             products.created_at   as product_created_at,
+             categories.id         as category_id,
+             categories.name       as category_name,
+             categories.created_at as category_created_at,
+             sellers.id            as seller_id,
+             sellers.username      as seller_username,
+             sellers.email         as seller_email,
+             sellers.password      as seller_password,
+             sellers.role          as seller_role,
+             sellers.created_at    as seller_created_at
+      FROM bids
+               INNER JOIN users ON bids.user_id = users.id
+               INNER JOIN products ON bids.product_id = products.id
+               INNER JOIN categories ON products.category_id = categories.id
+               INNER JOIN users AS sellers ON products.user_id = sellers.id;
+  `;
+  const rows: RawBidRecord[] = await execQuery( query );
+  return rows.map( ( row: RawBidRecord ): Bid => ( {
+    id:         row.id,
+    amount:     row.amount,
+    created_at: row.created_at,
+    user:       {
+      id:         row.user_id,
+      username:   row.username,
+      email:      row.email,
+      password:   row.password,
+      role:       row.role,
+      created_at: row.user_created_at,
+    },
+    product:    {
+      id:          row.product_id,
+      name:        row.product_name,
+      description: row.product_description,
+      price:       row.product_price,
+      created_at:  row.product_created_at,
+      category:    {
+        id:         row.category_id,
+        name:       row.category_name,
+        created_at: row.category_created_at,
+      },
+      seller:      {
+        id:         row.seller_id,
+        username:   row.seller_username,
+        email:      row.seller_email,
+        password:   row.seller_password,
+        role:       row.seller_role,
+        created_at: row.seller_created_at,
+      },
+    },
+  } ) );
+}
+
+export async function fetchTransactions (): Promise<Transaction[]>
+{
+  const query: string = `
+      SELECT transactions.id,
+             transactions.amount,
+             transactions.created_at,
+             bidders.id            as bidder_id,
+             bidders.username      as bidder_username,
+             bidders.email         as bidder_email,
+             bidders.password      as bidder_password,
+             bidders.role          as bidder_role,
+             bidders.created_at    as bidder_created_at,
+             sellers.id            as seller_id,
+             sellers.username      as seller_username,
+             sellers.email         as seller_email,
+             sellers.password      as seller_password,
+             sellers.role          as seller_role,
+             sellers.created_at    as seller_created_at,
+             products.id           as product_id,
+             products.name         as product_name,
+             products.description  as product_description,
+             products.price        as product_price,
+             products.created_at   as product_created_at,
+             categories.id         as category_id,
+             categories.name       as category_name,
+             categories.created_at as category_created_at
+      FROM transactions
+               INNER JOIN users AS bidders ON transactions.bidder_id = bidders.id
+               INNER JOIN users AS sellers ON transactions.seller_id = sellers.id
+               INNER JOIN products ON transactions.product_id = products.id
+               INNER JOIN categories ON products.category_id = categories.id;
+  `;
+  const rows: RawTransactionRecord[] = await execQuery( query );
+  return rows.map( ( row: RawTransactionRecord ): Transaction => ( {
+    id:         row.id,
+    amount:     row.amount,
+    created_at: new Date( row.created_at ),
+    bidder:     {
+      id:         row.bidder_id,
+      username:   row.bidder_username,
+      email:      row.bidder_email,
+      password:   row.bidder_password,
+      role:       row.bidder_role,
+      created_at: row.bidder_created_at,
+    },
+    seller:     {
+      id:         row.seller_id,
+      username:   row.seller_username,
+      email:      row.seller_email,
+      password:   row.seller_password,
+      role:       row.seller_role,
+      created_at: row.seller_created_at,
+    },
+    product:    {
+      id:          row.product_id,
+      name:        row.product_name,
+      description: row.product_description,
+      price:       row.product_price,
+      created_at:  row.product_created_at,
+      category:    {
+        id:         row.category_id,
+        name:       row.category_name,
+        created_at: row.category_created_at,
+      },
+      seller:      {
+        id:         row.seller_id,
+        username:   row.seller_username,
+        email:      row.seller_email,
+        password:   row.seller_password,
+        role:       row.seller_role,
+        created_at: row.seller_created_at,
+      },
+    },
+  } ) );
 }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,30 +1,63 @@
-export type EMail = `${string}@${string}.${string}` & string
-
 export enum Role
 {
   ADMIN = "admin",
   USER = "user"
 }
 
-export type RawUser = Omit<User, "email" | "role"> & { email: string, role: string }
-export type JoinedProduct = Omit<Product, "category" | "seller"> & {
+export interface JoinedSeller
+{
+  seller_id: number,
+  seller_username: string,
+  seller_email: string,
+  seller_password: string,
+  seller_role: Role & string,
+  seller_created_at: Date
+}
+
+export interface JoinedBidder
+{
+  bidder_id: number,
+  bidder_username: string,
+  bidder_email: string,
+  bidder_password: string,
+  bidder_role: Role & string,
+  bidder_created_at: Date
+}
+
+export interface JoinedCategory
+{
   category_id: number,
   category_name: string,
-  category_created_at: Date
-} & {
-  user_id: number,
-  user_username: string,
-  user_email: string,
-  user_password: string
+  category_created_at: Date,
 }
+
+export interface JoinedProduct
+{
+  product_id: number;
+  product_name: string;
+  product_description: string;
+  product_price: number;
+  product_created_at: Date;
+}
+
+export type RawProductRecord = Product & JoinedSeller & JoinedCategory
+export type RawBidRecord =
+  Bid
+  & Product
+  & JoinedCategory
+  & JoinedProduct
+  & JoinedSeller
+  & Omit<User, "id" | "created_at">
+  & { user_created_at: Date, user_id: number }
+export type RawTransactionRecord = Transaction & JoinedProduct & JoinedSeller & JoinedCategory & JoinedBidder
 
 export interface User
 {
   id: number;
   username: string;
-  email: EMail;
+  email: string;
   password: string;
-  role: Role;
+  role: Role | string;
   created_at: Date;
 }
 
