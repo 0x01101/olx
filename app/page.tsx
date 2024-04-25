1e726397e0fd25584d9bb26b022266f39704e886
JamJestJerzy <github@j3rzy.dev>
2024-04-25 15:51:02 +0200

    Updated dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import SearchBar from "@/app/ui/elements/searchbar";
 import Categories from "@/app/ui/elements/categories";
 import { CategoriesSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
-import { fetchCategories } from "@/app/lib/data/sql";
-import { Category } from "@/app/lib/definitions";
 
 export default function Page (  ): JSX.Element
 {
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,8 +1,18 @@
+import styles from "@/app/ui/elements/css/categories.module.css";
+
 const shimmer: string = "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
 
 export function CategoriesSkeleton (): JSX.Element {
   return (
-    <>
-    </>
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <div className={styles.categories}>
+          <div>
+            <div className="h-5 w-5 rounded-md bg-gray-200"/>
+            <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium"/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
