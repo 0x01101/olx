0a5269101a9a64c83c6f9f685eb8604c5e504f21
JamJestJerzy <github@j3rzy.dev>
2024-04-26 15:00:02 +0200

    Updated css

--- app/[categoryName]/page.tsx
+++ app/[categoryName]/page.tsx
@@ -1,5 +1,5 @@
 import { Category, Product } from "@/app/lib/definitions";
-import { fetchCategoriesByProperty, fetchNotifications, fetchProductsByProperty } from "@/app/lib/data/sql";
+import { fetchCategoriesByProperty, fetchProductsByProperty } from "@/app/lib/data/sql";
 import { notFound } from "next/navigation";
 import styles from "@/app/ui/css/category.module.css";
 
@@ -38,7 +38,14 @@ export default async function Page ( { params }: { params: { categoryName: strin
         </div>
         <div className={styles.miscOptionsContainer}>
           <MiscOption name={"photos"} text={"With photos only"} checked={withPhotosOnly}/>
-          <button type={"button"} data-testid="fav-search-btn" className={styles.watchSearch}></button>
+          <button type={"button"} data-testid="fav-search-btn" className={styles.watchSearch}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em"
+                 className={styles.watchSearchIcon}>
+              <path fill="currentColor" fillRule="evenodd"
+                    d="M20.219 10.367 12 20.419 3.806 10.4A3.96 3.96 0 0 1 3 8c0-2.206 1.795-4 4-4a4.004 4.004 0 0 1 3.868 3h2.264A4.003 4.003 0 0 1 17 4c2.206 0 4 1.794 4 4 0 .868-.279 1.698-.781 2.367M17 2a5.999 5.999 0 0 0-5 2.686A5.999 5.999 0 0 0 7 2C3.692 2 1 4.691 1 8a5.97 5.97 0 0 0 1.232 3.633L10.71 22h2.582l8.501-10.399A5.943 5.943 0 0 0 23 8c0-3.309-2.692-6-6-6"></path>
+            </svg>
+            Watch Search
+          </button>
         </div>
       </form>
     </div>
