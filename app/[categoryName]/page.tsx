a32f45c67bb3f0ae7e20606154cf0ed2073eec12
JamJestJerzy <github@j3rzy.dev>
2024-04-26 12:02:02 +0200

    Added misc option, checkpoint

--- app/[categoryName]/page.tsx
+++ app/[categoryName]/page.tsx
@@ -5,6 +5,8 @@ import styles from "@/app/ui/css/category.module.css";
 
 export default async function Page ( { params }: { params: { categoryName: string } } ): Promise<JSX.Element>
 {
+  const withPhotosOnly: boolean = false; // TODO: Add logic to actually change it's value
+  
   const categoryName: string = params.categoryName;
   const category: Category = ( await fetchCategoriesByProperty( "name", categoryName ) )[ 0 ];
   if ( !category ) notFound();
@@ -35,11 +37,30 @@ export default async function Page ( { params }: { params: { categoryName: strin
           <div></div>
         </div>
         <div className={styles.miscOptionsContainer}>
-          <div className={styles.onlyWithPhoto}>
-          
-          </div>
+          <MiscOption name={"photos"} text={"With photos only"} checked={withPhotosOnly}/>
         </div>
       </form>
     </div>
-);
+  );
+}
+
+function MiscOption ( { name, text, checked }: { name: string, text: string, checked: boolean } ): JSX.Element
+{
+  return (
+    <div className={styles.miscOption}>
+      <label data-testid="label" className={styles.miscOptionCheckboxLabel}>
+        <div
+          className={checked ? styles.miscOptionCheckboxContainerChecked : styles.miscOptionCheckboxContainerUnchecked}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" data-testid="tick"
+               className={styles.miscOptionCheckboxIcon}
+               style={checked ? { fillOpacity: 1 } : { fillOpacity: 0 }}>
+            <path fill="currentColor" fillRule="evenodd"
+                  d="m20.586 5-1.271 1.296L8 17.836l-3.315-3.38-1.271-1.297H2v1.443l1.271 1.296L7.293 20h1.414L20.73 7.738 22 6.442V5z"></path>
+          </svg>
+        </div>
+        <input name={name} data-testid="checkbox-field" id={name} type="checkbox"/>
+      </label>
+      <label htmlFor={"photos"} className={styles.miscOptionTextLabel}>{text}</label>
+    </div>
+  );
 }
