2fc47e625a326d7cf4212d57a335e4e4a24e9edf
JamJestJerzy <github@j3rzy.dev>
2024-04-29 10:39:12 +0200

    Moved dropdown icon to seperate .svg file

--- app/[categoryName]/page.tsx
+++ app/[categoryName]/page.tsx
@@ -5,6 +5,7 @@ import { capitalize } from "@/app/lib/text";
 import { fetchCategoryByName, fetchProductsInCategory } from "@/app/lib/data/fetch";
 import ListingCard from "@/app/ui/elements/categoryName/listingCard";
 import WatchSearchButton from "@/app/ui/elements/categoryName/watchSearchButton";
+import Image from "next/image";
 
 export default async function Page ( { params }: { params: { categoryName: string } } ): Promise<JSX.Element>
 {
@@ -57,7 +58,7 @@ export default async function Page ( { params }: { params: { categoryName: strin
                       <div className={styles.categoryFilterSelectedText}>
                         {capitalize( category.name )}
                       </div>
-                      <DropdownIcon />
+                      <Image src={"/app/static/media/dropdownIcon.svg"} alt={"Dropdown icon"} width={22} height={22}/>
                     </div>
                     {/* <div></div> TODO: Dropdown menu here */}
                   </div>
@@ -129,16 +130,5 @@ function MiscOption ( { name, text, checked }: { name: string, text: string, che
       </label>
       <label htmlFor={"photos"} className={styles.miscOptionTextLabel}>{text}</label>
     </div>
-  );
-}
-
-function DropdownIcon (): JSX.Element
-{
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em"
-         className={styles.dropdownIcon}>
-      <path fill="currentColor" fillRule="evenodd"
-            d="M2.001 6.5h1.414l1.27 1.271 7.316 7.315 7.315-7.315L20.587 6.5h1.414v1.414l-1.27 1.27-7.316 7.316-1 1h-.827l-3.942-3.942-4.374-4.374-1.27-1.27z"></path>
-    </svg>
   );
 }
