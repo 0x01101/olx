f08974d32b1fb1b85c9afb746ac642fabea6e717
JamJestJerzy <github@j3rzy.dev>
2024-04-30 18:26:38 +0200

    Fuck that shit

--- app/lib/data/sql.ts
+++ app/lib/data/sql.ts
@@ -44,32 +44,3 @@ export function mergeQuery ( baseQuery: string, toMerge: string ): string
   if ( baseQuery.trim().endsWith( ";" ) ) return baseQuery.replace( /;\s*$/im, ` ${toMerge};` );
   else return `${baseQuery} ${toMerge}`;
 }
-
-export class Table
-{
-  public script: string;
-  
-  public constructor ( tableConfig: TableConfig )
-  {
-    let script: string = "";
-    script += `create table ${tableConfig.name}` + `\n` + `(` + `\n`;
-    script += `id INT PRIMARY KEY AUTO_INCREMENT,`
-    script += tableConfig.rows.map( ( row: Row ): string =>
-    {
-      const typeObj: SQLType = row.type;
-      const useParenthesis: boolean = !!( typeObj.values || typeObj.size || typeObj.precision );
-      const type: string = `
-      ${typeObj.name}${useParenthesis ? "(" : ""}
-      ${typeObj.values || ""}
-      ${typeObj.size || ""}
-      ${typeObj.size && typeObj.precision ? ", " : ""}
-      ${typeObj.precision || ""}
-      ${typeObj.name}${useParenthesis ? ")" : ""}
-      `.replace( /\n/g, "" );
-      return `\t${row.name} ${type} ${row.default ? `default ${row.default}` : ""} ${row.unique ? "unique" : ""} ${row.notNull ? "not null" : ""}`;
-    } ).join( ",\n" );
-    script += `);`;
-    
-    this.script = script;
-  }
-}
