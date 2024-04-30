56a9fa1a0548dd5960ff1e31c80f53a1f641042c
JamJestJerzy <github@j3rzy.dev>
2024-04-30 12:30:22 +0200

    Changed <a> to <Link>

--- app/ui/elements/categories.tsx
+++ app/ui/elements/categories.tsx
@@ -2,6 +2,7 @@ import styles from "@/app/ui/elements/css/categories.module.css";
 import { Category } from "@/app/lib/definitions";
 import Image from "next/image";
 import { fetchCategories } from "@/app/lib/data/fetch";
+import Link from "next/link";
 
 export default async function Categories (): Promise<JSX.Element>
 {
@@ -16,12 +17,12 @@ export default async function Categories (): Promise<JSX.Element>
           <div data-testid="home-categories-menu-row" data-cy="home-categories-menu-row"
                className={styles.categories}>
             {categories.map( ( c: Category, i: number ) =>
-              ( <a className={styles.category} key={i} data-testid={`cat-${c.id}`} data-cy={`cat-${c.id}`}
+              ( <Link className={styles.category} key={i} data-testid={`cat-${c.id}`} data-cy={`cat-${c.id}`}
                    data-check={c.id} data-path={c.name.toLowerCase()} href={`/${c.name.toLowerCase()}/`}>
                 <Image src={`${c.logo_path}`}
                        alt={`${c.name} category image`} className={styles.categoryImage} width={88} height={88} />
                 <span className={styles.categoryName}>{c.name.trim()}</span>
-              </a> ) )
+              </Link> ) )
             }
           </div>
         </div>
