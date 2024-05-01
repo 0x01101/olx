1ce5a8fbcad7a04d219866bda1e2351b48b491e0
JamJestJerzy <github@j3rzy.dev>
2024-05-01 23:24:16 +0200

    Added fetchCategories

--- app/lib/sql/data/fetch.ts
+++ app/lib/sql/data/fetch.ts
@@ -1,7 +1,7 @@
-import { User } from "@/app/lib/definitions";
+import { Category, User } from "@/app/lib/definitions";
 import { execQuery, mergeQuery } from "@/app/lib/sql/sql";
 import { UserRecord } from "@/app/lib/sql/types/definitions";
-import { user } from "@/app/lib/sql/constants/queries";
+import { category, user } from "@/app/lib/sql/constants/queries";
 import { userSchema } from "@/app/lib/sql/types/schemas";
 import { SafeParseReturnType } from "zod";
 
@@ -12,4 +12,11 @@ export async function fetchUserByEMail ( email: string ): Promise<User | undefin
   const parsed: SafeParseReturnType<UserRecord, User> = userSchema.safeParse( rows[ 0 ] );
   if ( !parsed.success ) return undefined;
   return parsed.data;
+}
+
+export async function fetchCategories (): Promise<Category[] | undefined>
+{
+  const rows: Category[] = await execQuery<Category>( category );
+  if ( !rows.length ) return undefined;
+  return rows;
 }
