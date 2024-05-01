3e8428988e46abf3356e376ba3878d51e2d09599
JamJestJerzy <github@j3rzy.dev>
2024-05-01 22:03:07 +0200

    Changed fetchUserByEMail to use .safeParse() instead of .parse()

--- app/lib/sql/data/fetch.ts
+++ app/lib/sql/data/fetch.ts
@@ -3,10 +3,13 @@ import { execQuery, mergeQuery } from "@/app/lib/sql/sql";
 import { UserRecord } from "@/app/lib/sql/types/definitions";
 import { user } from "@/app/lib/sql/constants/queries";
 import { userSchema } from "@/app/lib/sql/types/schemas";
+import { SafeParseReturnType } from "zod";
 
 export async function fetchUserByEMail ( email: string ): Promise<User | undefined>
 {
   const rows: UserRecord[] = await execQuery<UserRecord>( mergeQuery( user, `WHERE email = ?` ), [ email ] );
   if ( !rows.length ) return undefined;
-  return userSchema.parse( rows[ 0 ] );
+  const parsed: SafeParseReturnType<UserRecord, User> = userSchema.safeParse( rows[ 0 ] );
+  if ( !parsed.success ) return undefined;
+  return parsed.data;
 }
