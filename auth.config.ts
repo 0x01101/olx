4470e03abe035a8c17a5d935260ec6d5aa7eb5c4
JamJestJerzy <github@j3rzy.dev>
2024-05-04 17:23:13 +0200

    Added some nextauth config

---
+++ auth.config.ts
@@ -0,0 +1,6 @@
+import GitHub from "@auth/core/providers/github";
+import { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [ GitHub ],
+} satisfies NextAuthConfig;
---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+const { auth } = NextAuth( authConfig );
+
+export default auth( ( req ) =>
+{
+  const isLoggedIn = !!req.auth;
+} );
+
+export const config = {
+  matcher: [ "/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)" ],
+};
