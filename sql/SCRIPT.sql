3c993c0d5bac3d193015d40f81daba340faf9a85
JamJestJerzy <github@j3rzy.dev>
2024-05-04 21:42:32 +0200

    Updated SQL

--- sql/SCRIPT.sql
+++ sql/SCRIPT.sql
@@ -7,35 +7,47 @@ drop user if exists funnyuser@'%';
 create user funnyuser@'%' identified by 'P@55w0rd';
 grant select, insert, update, delete, create, alter on olx.* to funnyuser@'%';
 
-CREATE TABLE `User` (
-                      `id` VARCHAR(255) NOT NULL PRIMARY KEY,
-                      `name` VARCHAR(255),
-                      `username` VARCHAR(255),
-                      `email` VARCHAR(255),
-                      `emailVerified` DATETIME,
-                      `image` VARCHAR(255),
-                      `password` VARCHAR(255),
-                      `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                      `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+create table User
+(
+  id            varchar(255) not null primary key default (uuid()),
+  name          varchar(255),
+  username      varchar(255) unique,
+  email         varchar(255) unique,
+  emailVerified datetime,
+  image         varchar(255),
+  password      varchar(255),
+  role          enum ('USER', 'ADMIN')            default 'USER',
+  createdAt     datetime                          default current_timestamp,
+  updatedAt     datetime                          default current_timestamp on update current_timestamp
 );
 
-CREATE TABLE `Account` (
-                         `id` VARCHAR(255) NOT NULL PRIMARY KEY,
-                         `userId` VARCHAR(255) UNIQUE,
-                         `type` VARCHAR(255),
-                         `provider` VARCHAR(255),
-                         `providerAccountId` VARCHAR(255),
-                         `refresh_token` TEXT,
-                         `access_token` TEXT,
-                         `expires_at` INT,
-                         `token_type` VARCHAR(255),
-                         `scope` VARCHAR(255),
-                         `id_token` TEXT,
-                         `session_state` VARCHAR(255),
-                         `refresh_token_expires_in` INT,
-                         `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                         FOREIGN KEY (`userId`) REFERENCES `User`(`id`),
-                         UNIQUE (`provider`, `providerAccountId`),
-                         INDEX (`userId`)
+create table Account
+(
+  id                       varchar(255) not null primary key default (uuid()),
+  userId                   varchar(255) unique,
+  type                     varchar(255),
+  provider                 varchar(255),
+  providerAccountId        varchar(255),
+  refresh_token            text,
+  access_token             text,
+  expires_at               int,
+  token_type               varchar(255),
+  scope                    varchar(255),
+  id_token                 text,
+  session_state            varchar(255),
+  refresh_token_expires_in int,
+  createdAt                datetime                          default current_timestamp,
+  updatedAt                datetime                          default current_timestamp on update current_timestamp,
+  foreign key (userId) references User (id),
+  unique (provider, providerAccountId),
+  index (userId)
+);
+
+create table VerificationToken
+(
+  id      varchar(255) not null primary key default (uuid()),
+  email   varchar(255),
+  token   varchar(255) unique,
+  expires datetime,
+  unique (email, token)
 );
