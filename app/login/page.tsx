111078daa9f8b8b1b71eb65542662158ad9912ed
JamJestJerzy <github@j3rzy.dev>
2024-05-04 13:03:59 +0200

    Cleared /login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,7 @@
-import styles from "@/app/ui/css/login.module.css";
-import LoginForm from "@/app/ui/elements/login/loginForm";
-import LoginLayout from "@/app/ui/layouts/loginLayout";
-
 export default async function Page (): Promise<JSX.Element>
 {
   return (
-    <LoginLayout>
-      <div className={styles.grid}>
-        <div className={styles.outerContainer}>
-          <div className={styles.container}>
-            <LoginForm />
-          </div>
-        </div>
-      </div>
-    </LoginLayout>
+    <>
+    </>
   );
 }
