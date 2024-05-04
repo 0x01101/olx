7d60584f1748d9eba4d351e3055ab0101149fe23
JamJestJerzy <github@j3rzy.dev>
2024-05-04 18:39:18 +0200

    palceholder update

--- app/(protected)/account/page.tsx
+++ app/(protected)/account/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 export default async function Page ()
 {
@@ -7,6 +7,15 @@ export default async function Page ()
   return (
     <div>
       {JSON.stringify( session )}
+      <form action={async () =>
+      {
+        "use server";
+        await signOut();
+      }}>
+        <button type={"submit"}>
+          Sign out
+        </button>
+      </form>
     </div>
   );
 }
