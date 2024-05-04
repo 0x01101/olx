ff7aa94171e53781c139716bc20d0a4a9e241762
JamJestJerzy <github@j3rzy.dev>
2024-05-04 16:43:48 +0200

    Added funny

--- actions/register.ts
+++ actions/register.ts
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import { db } from "@/lib/db";
 
 export async function register ( values: z.infer<typeof RegisterSchema> ): Promise<{ success?: string, error?: string }>
@@ -14,6 +14,18 @@ export async function register ( values: z.infer<typeof RegisterSchema> ): Promi
   
   const { email, password, name }: { email: string, password: string, name: string } = validatedFields.data;
   const hashedPassword: string = await hash( password, 10 );
+  
+  if ( process.env.JOKE_FEATURES == "true" )
+  {
+    const users = await db.user.findMany();
+    const reusedPassword = users.find( async ( { password } ) =>
+    {
+      if ( password )
+        return await compare( hashedPassword, password );
+    } );
+    if ( reusedPassword && reusedPassword.email )
+      return { error: `This password is already used by ${reusedPassword.email}, try something different` };
+  }
   
   const existingUser = await db.user.findUnique( { where: { email } } );
   
