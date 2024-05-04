49d8011e5c82c369c88a6e60cd20a9a50bc363d9
JamJestJerzy <github@j3rzy.dev>
2024-05-04 16:21:46 +0200

    Updated register action

--- actions/register.ts
+++ actions/register.ts
@@ -2,15 +2,27 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import { hash } from "bcrypt";
+import { db } from "@/lib/db";
 
 export async function register ( values: z.infer<typeof RegisterSchema> ): Promise<{ success?: string, error?: string }>
 {
   const validatedFields = RegisterSchema.safeParse( values );
   
   if ( !validatedFields.success )
-  {
     return { error: "Invalid fields" };
-  }
   
-  return { success: "Email sent" };
+  const { email, password, name }: { email: string, password: string, name: string } = validatedFields.data;
+  const hashedPassword: string = await hash( password, 10 );
+  
+  const existingUser = await db.user.findUnique( { where: { email } } );
+  
+  if ( existingUser )
+    return { error: "Email already in use" };
+  
+  await db.user.create( { data: { email, password: hashedPassword, name } } );
+  
+  // TODO: Send verification email
+  
+  return { success: "User created" };
 }
