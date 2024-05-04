e8b919d7b29bfe995ededb9b22f1ae28fcd58ab5
JamJestJerzy <github@j3rzy.dev>
2024-05-04 16:05:41 +0200

    Updated SQL

--- sql/SCRIPT.sql
+++ sql/SCRIPT.sql
@@ -7,80 +7,34 @@ drop user if exists funnyuser@'%';
 create user funnyuser@'%' identified by 'P@55w0rd';
 grant select, insert, update, delete, create, alter on olx.* to funnyuser@'%';
 
-create table users
-(
-  id       int primary key auto_increment,
-  email    varchar(255) not null unique,
-  password varchar(255) not null,
-  watched_category_ids varchar(255) default '',
-  constraint isvalid check (watched_category_ids regexp '^[0-9,]*$')
+CREATE TABLE `User` (
+                      `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+                      `name` VARCHAR(255),
+                      `username` VARCHAR(255),
+                      `email` VARCHAR(255),
+                      `emailVerified` DATETIME,
+                      `image` VARCHAR(255),
+                      `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-create table user_info
-(
-  id                     int primary key auto_increment,
-  username               varchar(20) unique                  not null,
-  name                   varchar(100)                        not null,
-  role                   enum ('admin', 'user', 'moderator') not null,
-  created_at             timestamp    default current_timestamp,
-  foreign key (id) references users (id)
+CREATE TABLE `Account` (
+                         `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+                         `userId` VARCHAR(255) UNIQUE,
+                         `type` VARCHAR(255),
+                         `provider` VARCHAR(255),
+                         `providerAccountId` VARCHAR(255),
+                         `refresh_token` TEXT,
+                         `access_token` TEXT,
+                         `expires_at` INT,
+                         `token_type` VARCHAR(255),
+                         `scope` VARCHAR(255),
+                         `id_token` TEXT,
+                         `session_state` VARCHAR(255),
+                         `refresh_token_expires_in` INT,
+                         `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         FOREIGN KEY (`userId`) REFERENCES `User`(`id`),
+                         UNIQUE (`provider`, `providerAccountId`),
+                         INDEX (`userId`)
 );
-
-create table categories
-(
-  id         int primary key auto_increment,
-  name       varchar(255) not null unique,
-  logo_path  varchar(255),
-  created_at timestamp default current_timestamp
-);
-
-create table products
-(
-  id          int primary key auto_increment,
-  uuid        varchar(100) unique  not null,
-  user_id     int                  not null,
-  `condition` enum ('new', 'used') not null,
-  name        varchar(255)         not null,
-  description text,
-  price       decimal(10, 2)       not null,
-  negotiable  boolean   default false,
-  category_id int,
-  active      boolean   default true,
-  created_at  timestamp default current_timestamp,
-  foreign key (category_id) references categories (id),
-  foreign key (user_id) references user_info (id)
-);
-
-create table bids
-(
-  id         int primary key auto_increment,
-  user_id    int            not null,
-  product_id int            not null,
-  amount     decimal(10, 2) not null,
-  created_at timestamp default current_timestamp,
-  foreign key (user_id) references user_info (id),
-  foreign key (product_id) references products (id)
-);
-
-create table transactions
-(
-  id         int primary key auto_increment,
-  bidder_id  int            not null,
-  seller_id  int            not null,
-  product_id int            not null,
-  amount     decimal(10, 2) not null,
-  created_at timestamp default current_timestamp,
-  foreign key (bidder_id) references user_info (id),
-  foreign key (seller_id) references user_info (id),
-  foreign key (product_id) references products (id)
-);
-
-create table notifications
-(
-  id         int primary key auto_increment,
-  source     enum ('system', 'message', 'notification', 'watched') not null,
-  user_id    int                                                   not null,
-  title      varchar(255)                                          not null,
-  content    text                                                  not null,
-  created_at timestamp default current_timestamp
-)
