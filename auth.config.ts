d9864eb9b2c2e1985c0588b7820ac90a02cb9313
JamJestJerzy <github@j3rzy.dev>
2024-05-04 18:14:42 +0200

    Added some more authjs config

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,30 @@
-import GitHub from "@auth/core/providers/github";
+import Credentials from "@auth/core/providers/credentials";
 import { NextAuthConfig } from "next-auth";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import bcrypt from "bcrypt";
 
 export default {
-  providers: [ GitHub ],
+  providers: [
+    Credentials( {
+      async authorize ( credentials )
+      {
+        const validatedFields = LoginSchema.safeParse( credentials );
+        
+        if ( validatedFields.success )
+        {
+          const { email, password }: { email: string, password: string } = validatedFields.data;
+          
+          const user = await getUserByEmail( email );
+          if ( !user || !user.password ) return null;
+          
+          const passwordsMatch = await bcrypt.compare( password, user.password );
+          
+          if ( passwordsMatch ) return user;
+        }
+        
+        return null;
+      },
+    } ),
+  ],
 } satisfies NextAuthConfig;
