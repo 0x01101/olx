0aa229b837d7222f33d5d4a1c3e0f6048b556e73
JamJestJerzy <github@j3rzy.dev>
2024-05-04 21:55:22 +0200

    Added function to generate verification token

---
+++ lib/tokens.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { VerificationToken } from "@prisma/client";
+import { db } from "@/lib/db";
+
+export async function generateVerificationToken ( email: string ): Promise<VerificationToken>
+{
+  const token: string = uuidv4();
+  const expires: Date = new Date( new Date().getTime() + 3600 * 1000 );
+  
+  const existingToken: VerificationToken | null = await getVerificationTokenByEmail( email );
+  
+  if ( existingToken )
+    await db.verificationToken.delete( { where: { id: existingToken.id } } );
+  
+  return db.verificationToken.create( {
+    data: {
+      email,
+      token,
+      expires,
+    },
+  } );
+}
