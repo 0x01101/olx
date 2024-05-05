ef10b2de5cd632a8d8992f2dfa433f92db67ac04
JamJestJerzy <github@j3rzy.dev>
2024-05-05 12:09:15 +0200

    Added generator function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
-import { VerificationToken } from "@prisma/client";
+import { PasswordResetToken, VerificationToken } from "@prisma/client";
 import { db } from "@/lib/db";
+import { getPasswordResetTokenByEMail } from "@/data/password-reset-token";
 
 export async function generateVerificationToken ( email: string ): Promise<VerificationToken>
 {
@@ -14,6 +15,25 @@ export async function generateVerificationToken ( email: string ): Promise<Verif
     await db.verificationToken.delete( { where: { id: existingToken.id } } );
   
   return db.verificationToken.create( {
+    data: {
+      email,
+      token,
+      expires,
+    },
+  } );
+}
+
+export async function generatePasswordResetToken ( email: string ): Promise<PasswordResetToken>
+{
+  const token: string = uuidv4();
+  const expires: Date = new Date( new Date().getTime() + 3600 * 1000 );
+  
+  const existingToken: PasswordResetToken | null = await getPasswordResetTokenByEMail( email );
+  
+  if ( existingToken )
+    await db.passwordResetToken.delete( { where: { id: existingToken.id } } );
+  
+  return db.passwordResetToken.create( {
     data: {
       email,
       token,
