64b8d6d503d61c33ed1fc95892b5d5e0ceadef1c
JamJestJerzy <github@j3rzy.dev>
2024-05-05 21:09:41 +0200

    mariadb -> prisma migration

--- app/[categoryName]/page.tsx
+++ app/[categoryName]/page.tsx
@@ -5,19 +5,19 @@ import ListingCard from "@/app/ui/elements/categoryName/listingCard";
 import WatchSearchButton from "@/app/ui/elements/categoryName/watchSearchButton";
 import Image from "next/image";
 import MainLayout from "@/app/ui/layouts/mainLayout";
-import { Category } from "@prisma/client";
+import { Category, Product } from "@prisma/client";
 import { db } from "@/lib/db";
 
 export default async function Page ( { params }: { params: { categoryName: string } } ): Promise<JSX.Element>
 {
   const categoryName: string = params.categoryName.trim();
-  let category: Category | undefined = await db.category.findUnique( { where: { name: categoryName } } ); // TODO: Make so you can change it via category dropdown
+  let category: Category | null = await db.category.findUnique( { where: { name: categoryName } } ); // TODO: Make so you can change it via category dropdown
   if ( !category ) notFound();
-  const products: Product[] | undefined = await fetchProductsInCategory( category );
+  const products: Product[] | undefined = await db.product.findMany( { where: { category: category } } );
   if ( !products ) notFound(); // TODO: Change
   
   let withPhotosOnly: boolean = false; // TODO: Add logic to actually change it's value
-  let watchingSearch: boolean = await isWatching( category ); // TODO: Add logic
+  let watchingSearch: boolean = false; // TODO: Add logic
   
   return (
     <MainLayout>
