3342f5e24abf6632bbe6aef09bd4dcda1f8fadbb
JamJestJerzy <github@j3rzy.dev>
2024-05-05 20:41:24 +0200

    Moved type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,19 +1,22 @@
 import { DefaultSession } from "next-auth";
 import { UserRole } from "@prisma/client";
 
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole
+}
+
 declare module "next-auth"
 {
   interface Session
   {
-    user: {
-      role: UserRole
-    } & DefaultSession["user"];
+    user: ExtendedUser;
   }
 }
 
 declare module "@auth/core/jwt"
 {
-  interface JWT {
+  interface JWT
+  {
     role?: UserRole;
   }
 }
