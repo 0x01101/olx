864cd1ebf0d80e18f41619513392262cce5d647b
JamJestJerzy <github@j3rzy.dev>
2024-05-05 17:14:48 +0200

    Moved to client component

--- app/(protected)/account/page.tsx
+++ app/(protected)/account/page.tsx
@@ -1,17 +1,15 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-export default async function Page ()
+import { useSession } from "next-auth/react";
+
+export default function Page ()
 {
-  const session = await auth();
+  const session = useSession();
   
   return (
     <div>
       {JSON.stringify( session )}
-      <form action={async () =>
-      {
-        "use server";
-        await signOut();
-      }}>
+      <form>
         <button type={"submit"}>
           Sign out
         </button>
