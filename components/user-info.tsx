f52a6211248b4381c16c3f1d9a134180d7f03449
JamJestJerzy <github@j3rzy.dev>
2024-05-05 21:57:29 +0200

    Changed env var names

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -1,6 +1,17 @@
 import { ExtendedUser } from "@/next-auth";
+import { Card } from "@/components/ui/card";
 
 interface UserInfoProps
 {
-  user: ExtendedUser;
+  user?: ExtendedUser;
+  label: string;
+}
+
+export function UserInfo ( { user, label }: UserInfoProps ): JSX.Element
+{
+  return (
+    <Card>
+    
+    </Card>
+  )
 }
