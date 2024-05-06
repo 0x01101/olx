a7c8fc4afc11ebd7d3cf059d2b490a29097e5681
JamJestJerzy <github@j3rzy.dev>
2024-05-06 13:41:51 +0200

    I dunno

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,4 +1,16 @@
+import { Product } from "@prisma/client";
+
 export interface ServerResponse {
   success?: string;
   error?: string;
 }
+
+export interface UserDTO
+{
+  id: string;
+  name?: string;
+  image?: string;
+  createdAt: Date;
+}
+
+export type ProductDTO = Omit<Product, "">
