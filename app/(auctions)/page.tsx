3f2b822664ce049eaf85045e4d2425de4ede76dc
JamJestJerzy <github@j3rzy.dev>
2024-05-06 23:05:29 +0200

    Reorganized

--- app/(auctions)/page.tsx
+++ app/(auctions)/page.tsx
@@ -1,11 +1,13 @@
-import Categories from "@/components/categories";
+import Categories from "@/components/main/categories";
 import { Suspense } from "react";
 import { CategoriesSkeleton } from "@/components/skeletons";
+import { ProductSearchBar } from "@/components/main/product-searchbar";
 
 export default async function Page (): Promise<JSX.Element>
 {
   return (
     <div className={"bg-primary overflow-hidden p-0 m-0 relative"}>
+      <ProductSearchBar />
       <Suspense fallback={<CategoriesSkeleton />}>
         <Categories />
       </Suspense>
---
+++ components/main/product-searchbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export function ProductSearchBar (): JSX.Element
+{
+  return (
+    <div className="w-[calc(100% - 24px)] my-[12px] mx-[12px] bg-muted py-14 rounded-xl">
+    
+    </div>
+  );
+}
