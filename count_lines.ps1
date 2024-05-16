25263f9491c0b8895ec55a03765adf1e50bd3e4f
JamJestJerzy <github@j3rzy.dev>
2024-05-16 19:57:02 +0200

    Update

--- count_lines.ps1
+++ count_lines.ps1
@@ -1,8 +1,8 @@
-$excludeDirs = @("node_modules", ".git", ".idea", ".next", "components/ui")
+$excludeDirs = @("node_modules", ".git", ".idea", ".next", "components/ui", "assets/images", "package-lock.json", "sql/ERDiagram.png")
 
 function IsExcluded($path) {
     foreach ($excludeDir in $excludeDirs) {
-        if ($path -like "*\$excludeDir\*") {
+        if ($path -like "*\$excludeDir*") {
             return $true
         }
     }
@@ -13,11 +13,14 @@ $files = Get-ChildItem -Recurse -File
 
 $totalLineCount = 0
 
+$fileLineCounts = @{}
+
 foreach ($file in $files) {
     if (-not (IsExcluded $file.FullName)) {
         try {
             $lineCount = Get-Content -LiteralPath $file.FullName | Measure-Object -Line
             $totalLineCount += $lineCount.Lines
+            $fileLineCounts[$file.FullName] = $lineCount.Lines
         } catch {
             Write-Output ("Failed to read file: $($file.FullName). Error: $($_.Exception.Message)")
         }
@@ -25,3 +28,8 @@ foreach ($file in $files) {
 }
 
 Write-Output ("Total lines: $totalLineCount")
+
+$topFiles = $fileLineCounts.GetEnumerator() | Sort-Object -Property Value -Descending | Select-Object -First 3
+
+Write-Output ("Top 3 files with most lines:")
+$topFiles | ForEach-Object { Write-Output ("$($_.Key): $($_.Value) lines") }
