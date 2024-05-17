47b705582662467f757d5b08d5dc5d244bfdb945
JamJestJerzy <github@j3rzy.dev>
2024-05-17 22:53:39 +0200

    Added funny icon

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { dosis } from "@/lib/fonts";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { DashboardIcon } from "@radix-ui/react-icons";
 
 interface EntryProps
 {
@@ -20,30 +21,39 @@ export function SideBar (): JSX.Element
       paddings={false}
       className={"relative top-0 left-0 w-[180px] h-full flex flex-col items-center space-y-3 p-3"}
     >
-      <Link href={"/dashboard"} className={"w-full bg-primary rounded-md p-2"}>
+      <Link
+        href={"/dashboard"}
+        className={"w-full bg-primary rounded-md p-2 flex flex-row space-x-2 text-center items-center justify-center"}
+      >
+        <DashboardIcon className={"w-6 h-6"} />
         <p className={cn( "font-bold text-2xl text-center", dosis.className )}>Dashboard</p>
       </Link>
       <Entry href={"products"}>
         Products
       </Entry>
+      <Entry href={"categories"}>
+        Categories
+      </Entry>
     </Widget>
-  )
+  );
 }
 
 function Entry ( { children, href }: EntryProps ): JSX.Element
 {
   const pathname: string = usePathname();
-  const path: string = `/dashboard/${href}`
+  const path: string = `/dashboard/${href}`;
   
   return (
     <Link
       className={cn(
-        "w-full rounded-md p-2 text-center hover:scale-110 transition-all",
-        pathname === path ? "bg-emerald-700" : "bg-popover"
+        "w-full rounded-md p-2 hover:scale-110 transition-all",
+        pathname === path ? "bg-emerald-700" : "bg-popover",
       )}
       href={path}
     >
-      {children}
+      <div className={"flex flex-row space-x-2 w-full h-full text-center items-center justify-center"}>
+        {children}
+      </div>
     </Link>
-  )
+  );
 }
