d2258b4ed29fff944b09ed1dfb5474ad52c88cac
JamJestJerzy <github@j3rzy.dev>
2024-05-18 18:39:19 +0200

    Temporarly changed main dashboard page

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,9 +1,35 @@
 import { Widget } from "@/components/widget";
+import { Categories } from "@/components/dashboard/categories/categories";
+import { db } from "@/lib/db";
+import { FullCategory, FullProduct } from "@/lib/definitions";
+import { User } from "@prisma/client";
+import { Products } from "@/components/dashboard/products/products";
+import { Users } from "@/components/dashboard/users/users";
 
-export default async function Page(): Promise<JSX.Element> {
+export default async function Page (): Promise<JSX.Element>
+{
+  const categories: FullCategory[] = await db.category.findMany( { include: { Product: true } } );
+  const products: FullProduct[] = await db.product.findMany( {
+    include: {
+      seller:   true,
+      category: true,
+      images:   true,
+    },
+  } );
+  const users: User[] = await db.user.findMany();
+  
   return (
-    <Widget>
-      <>gdfg</>
-    </Widget>
+    <>
+      {/* TODO: CHANGE */}
+      <Widget title={"Categories"}>
+        <Categories categories={categories} />
+      </Widget>
+      <Widget title={"Products"}>
+        <Products products={products} categories={categories} users={users} />
+      </Widget>
+      <Widget title={"Users"}>
+        <Users users={users} />
+      </Widget>
+    </>
   );
 }
